7c5dbb41476c318ca5ee4d0fb15fcc943ee6aa28
Eliana Romero <79712030+Eli2032@users.noreply.github.com>
2021-04-22 23:15:03 -0500

    Add files via upload

---
+++ factura.js
@@ -0,0 +1,25 @@
+var Electricaribe = {
+
+   Factura: {
+        Usuario: {
+            Nombre: "Orlando",
+            Apellido: "Garcia",
+            Estrato: 2,
+            Direccion: "CR 7W 36A-31",
+            DirecciondeEnvio: "CR 7W 36A-31"
+        },
+        informaciondePago: {
+            Nic: 5869133,
+            TotalaPagarMes: 24.000,
+            TotalfacturasporPagar: 196499,
+            Numerofacturasvencidas: 8,
+            resumenFacturacion: {
+                Energia: 21820,
+                Aseo: 0,
+                AlumbradoPublico: 2810,
+                Seguridad: 0,
+                Total: 24630,
+            }
+        }
+    }
+};
